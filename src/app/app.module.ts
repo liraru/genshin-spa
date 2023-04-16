fe73946e688a8fc7a51130038a3c0877fa3b7cca
Lorena Sineiro <losingar@gmail.com>
2023-04-16 23:03:45 +0200

    base layout, personalized font, basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuModule } from './modules/main-menu/main-menu.module';
+import { NavbarModule } from './modules/navbar/navbar.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MainMenuModule, NavbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
