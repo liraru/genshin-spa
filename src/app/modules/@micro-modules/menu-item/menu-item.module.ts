07ccf9ccffd55d1aef0d259c0bd643125939e9b4
Lorena Sineiro <losingar@gmail.com>
2023-04-17 23:26:46 +0200

    wip menu modules and components

---
+++ src/app/modules/@micro-modules/menu-item/menu-item.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+
+@NgModule({
+  declarations: [MenuItemComponent],
+  imports: [CommonModule]
+})
+export class MenuItemModule {}
